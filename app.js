95375248600712766b9bbe5b7f737de3f9f9d77a
Smgy94 <Shanemcgeown@gmail.com>
2023-11-16 15:38:09 +0000

    Creating the Etch-A-Sketch Design and enhanced function
    HTML:
    - etch-a-sketch favicon
    -added thumb-stick circle
    - added an input field to get user input for grid size
    - added button to shake and clear board
    - linked animate.css stylesheet
    
    CSS:
    - styled the grid wrapper to look like an etch-a-sketch board
    - styled thumb-stick  circles
    - styled focus-visible state of buttons
    - implemented mobile-responsive design using media queries
    
    JS:
    - defined global variables so they can be reused in functions.
    - changed the prompt limit to between 2-50
    - updated each square to have a light grey outline
    - Added a 'shake the board' event that adds an animation to the boards and clears out the grid
    
    I added a README.md file

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const btn = document.querySelector("button");
+const btn = document.getElementById("btn-number");
+const btnClearTheBoard = document.getElementById("clear-board");
+const h1 = document.getElementById("h1");
+let gridWrapper = document.querySelector(".grid-wrapper");
+let circles = document.querySelectorAll(".circle");
 let num;
 let RGBColor;
+let square;
 
 // Prompt user for a number and validate number
 function handleButtonClick() {
-  let userSquares = prompt("Please enter a number bewtween 2 - 100");
+  let userSquares = prompt("Please enter a number bewtween 2 - 50");
   num = parseInt(userSquares);
 
   if (isValidNumber(num)) {
     populateBoard(num);
   } else {
-    alert("Error: Please enter a number bewtween 2 - 100");
+    alert("Error: Please enter a number bewtween 2 - 50");
   }
 
   function isValidNumber(num) {
-    return !isNaN(num) && num >= 2 && num <= 100;
+    return !isNaN(num) && num >= 2 && num <= 50;
   }
 }
+
 // Generate a random color for the each square
 function randomRGB() {
   let x = Math.floor(Math.random() * 256);
@@ -30,7 +36,7 @@ btn.addEventListener("click", handleButtonClick);
 
 // Create a grid layout using the user input num for columns & rows
 function populateBoard(num) {
-  let gridWrapper = document.querySelector(".grid-wrapper");
+  gridWrapper = document.querySelector(".grid-wrapper");
   let squares = gridWrapper.querySelectorAll("div");
   squares.forEach((div) => {
     div.remove();
@@ -41,16 +47,46 @@ function populateBoard(num) {
   let amount = num * num;
 
   for (let i = 0; i < amount; i++) {
-    let square = document.createElement("div");
-    square.style.outline = "1px solid black";
+    square = document.createElement("div");
+    square.style.outline = "rgba(0, 0, 0, .11) solid 1px";
     gridWrapper.insertAdjacentElement("beforeend", square);
 
-    square.addEventListener("mouseover", (event) => {
+    square.addEventListener("pointerover", (event) => {
       event.target.style.backgroundColor = `${RGBColor}`;
       randomRGB();
     });
-    square.addEventListener("mouseout", (event) => {
-      event.target.style.backgroundColor = "transparent";
-    });
   }
+
+  btn.placeholder = `Enter Grid Size: ${num} x ${num}`;
 }
+
+// Shake The Board  and Clean it
+btnClearTheBoard.addEventListener("click", () => {
+  setTimeout(() => {
+    gridWrapper.innerHTML = "";
+
+    h1.classList.remove("animate__animated");
+    h1.classList.remove("animate__shakeX");
+
+    gridWrapper.classList.remove("animate__animated");
+    gridWrapper.classList.remove("animate__shakeX");
+
+    circles.forEach((circle) => {
+      circle.classList.remove("animate__animated");
+      circle.classList.remove("animate__shakeX");
+    });
+  }, 1000);
+
+  gridWrapper.style.gridTemplateColumns = `repeat(${num},1fr)`;
+  gridWrapper.style.gridTemplateRows = `repeat(${num},1fr)`;
+  h1.classList.add("animate__animated");
+  h1.classList.add("animate__shakeX");
+
+  gridWrapper.classList.add("animate__animated");
+  gridWrapper.classList.add("animate__shakeX");
+
+  circles.forEach((circle) => {
+    circle.classList.add("animate__animated");
+    circle.classList.add("animate__shakeX");
+  });
+});
